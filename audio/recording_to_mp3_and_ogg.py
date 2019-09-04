5476598494ff19db4b1dcc296ee2174b7c6487e6
Sarah <48268016+sarahosse@users.noreply.github.com>
2019-09-04 10:50:41 +1000

    Create recording_to_mp3_and_ogg.py
    Testing the sound quality and file sizes of recordings exported/resampled to MP3 and OGG rather than FLAC.

---
+++ audio/recording_to_mp3_and_ogg.py
@@ -0,0 +1,78 @@
+import pyaudio      # Connects with Portaudio for audio device streaming
+import wave         # Used to save audio to .wav files
+import collections  # Used for Double ended Queue (deque) structure
+from scipy import signal
+import numpy
+from pydub import AudioSegment
+
+
+
+# Define Audio characteristics
+# ----------------------------------------------------------------------------------------
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 48000
+CHUNK = 1024
+RECORD_SECONDS = 2
+
+# have some function for pulling long/lat
+#longLat = { 0, 0}
+#UserID = 12345
+
+
+# Audio Recording
+# ----------------------------------------------------------------------------------------
+audio = pyaudio.PyAudio()
+
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,rate=RATE, input=True,frames_per_buffer=CHUNK)
+print ('recording...')
+
+data = stream.read(int(RATE/CHUNK) * CHUNK * RECORD_SECONDS)
+print ('finished recording')
+
+# stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+
+# Exporting / resampling
+# ----------------------------------------------------------------------------------------
+
+# we can add some metadata to the recording
+# recording.export("raw.mp3", format="mp3", tags={'LongLat': longLat, 'User': 12345, 'Priority': 'High!'})
+
+
+# Pydub addition - will export raw audio - data - into specified formats.
+recording = AudioSegment(data, sample_width=2, frame_rate=RATE, channels=1)
+recording.export("raw.wave", format="wav")     # Raw audio at 161KB per 10 seconds
+recording.export("raw.flac", format = "flac")  # ~50% reduction in file size; 91KB per 10 seconds
+recording.export("raw.mp3", format = "mp3")
+recording.export("raw.ogg", format = "ogg")
+
+recording_samples = recording.get_array_of_samples()
+
+
+# SciPy Resampling
+recording_resample = signal.resample(recording_samples, 8000)
+
+resamples = b''
+for i in range(0, len(recording_resample)):
+    resamples += (int(recording_resample[i].astype(numpy.int16)).to_bytes(8, byteorder='big', signed=True))
+print(resamples)
+
+newrecording = AudioSegment(resamples, sample_width=2, frame_rate=8000, channels=1)
+newrecording.export("resample.wav", format="wav")     # Raw audio at 161KB per 10 seconds
+newrecording.export("resample.flac", format = "flac")  # ~50% reduction in file size; 91KB per 10 seconds
+newrecording.export("resample.mp3", format = "mp3")
+newrecording.export("resample.ogg", format = "ogg")
+#newrecording.export(AAC_OUTPUT_FILENAME, format = "aac")   # Currently produces an error - unresolved github issue
+
+# Desktop conversion results in 53KB; ~70% reduction
+# C:\ > ffmpeg -i file.wav -codec:a aac file.aac 
+
+print('Done export')
+
+# ----------------------------------------------------------------------------------------
